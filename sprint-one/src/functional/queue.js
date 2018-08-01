7442b58043a8a9e583ba521974abe892d81a0ec8
Arthur Tharp <atharp@uci.edu>
2018-08-01 12:38:12 -0700

    queue and stack passing all tests

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,43 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var queue = 0;
+  var queueValue;
   // Implement the methods below
-
+  //beginning of the queue
+  var valDequed = 1;
   someInstance.enqueue = function(value) {
+    queue++;
+    storage[valDequed] = value;
+    someInstance[queue] = value;
   };
 
   someInstance.dequeue = function() {
+    queueValue = storage[queue];
+    if (queue === 1) {
+      delete someInstance[queue];
+      queue--;
+    }
+
+    else if (queue === 0) {
+      queue = 0;
+    }
+
+    else if (queue >= 1) {
+      queueValue = storage[valDequed-1];
+      if(queueValue === undefined) {
+        queueValue = someInstance[1];
+      }
+      delete someInstance[valDequed];
+      queue--;
+      valDequed++;
+    }
+    return queueValue;
+
   };
 
   someInstance.size = function() {
+    return queue;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,26 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var counter = 0
   // Implement the methods below
   someInstance.push = function(value) {
+    counter++;
+    someInstance[counter] = value;
   };
 
   someInstance.pop = function() {
+    //remove last added element
+    var value = someInstance[counter];
+    if (counter > 0) {
+
+      delete someInstance[counter];
+      counter--;
+    }
+    return value;
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
